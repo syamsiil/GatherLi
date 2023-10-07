0561cff7aac7263f88c912c061e784635c7e003a
syamsiil <muhammadalisyamsi@gmail.com>
2023-10-08 05:30:58 +0700

    feat: add slider in home page

--- FE/src/components/Card.tsx
+++ FE/src/components/Card.tsx
@@ -10,47 +10,86 @@ import {
 } from "@chakra-ui/react";
 
 import dummy from "../utils/dummyCardHome.json";
+import Slider from "react-slick";
 
 export default function CardHome() {
   const limitedData = dummy.slice(0, 4);
 
+  const settings = {
+    dots: true, // Tampilkan titik navigasi
+    infinite: true, // Membuat carousel berputar secara tak terbatas
+    speed: 500, // Kecepatan animasi (milidetik)
+    slidesToShow: 3, // Jumlah kartu yang ditampilkan dalam satu waktu
+    slidesToScroll: 1, // Jumlah kartu yang digulirkan setiap kali
+    initialSlide: 0, // Indeks kartu awal
+    responsive: [
+      {
+        breakpoint: 1024, // Breakpoint untuk tampilan layar lebar
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // Breakpoint untuk tampilan layar menengah
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
-      {limitedData.map((item) => (
-        <Card maxW="sm">
-          <Box>
-            <Image
-              src={item.Image}
-              borderRadius={"6px 6px 0 0"}
-              padding={0}
-              width={"100%"}
-              height={"200px"}
-              object-fit={"cover"}
-              object-position={"center"}
-            />
-            <Stack mt="6" spacing="3" padding={"0 20px"} textAlign={"justify"}>
-              <Heading size="md">{item.Title}</Heading>
-              <Text noOfLines={3}>{item.Description}</Text>
-            </Stack>
-          </Box>
+      <Slider key={"slider"} {...settings}>
+        {limitedData.map((item, index) => (
+          <Box
+            key={index}
+            display="flex"
+            justifyContent="center" // Mengatur posisi kartu ke tengah
+            maxWidth="370px"
+            margin="0 10px 20px 10px"
+          >
+            <Card flexWrap="wrap" flex="1" maxWidth="370px">
+              <Box>
+                <Image
+                  src={item.Image}
+                  borderRadius={"6px 6px 0 0"}
+                  padding={0}
+                  width={"100%"}
+                  height={"200px"}
+                  object-fit={"cover"}
+                  object-position={"center"}
+                />
+                <Stack
+                  mt="6"
+                  spacing="3"
+                  padding={"0 20px"}
+                  textAlign={"justify"}
+                >
+                  <Heading size="md">{item.Title}</Heading>
+                  <Text noOfLines={3}>{item.Description}</Text>
+                </Stack>
+              </Box>
 
-          <CardFooter>
-            <Button
-              variant="solid"
-              bgColor={"teal"}
-              width={"100%"}
-              colorScheme="none"
-              color={"white"}
-              bottom={"0"}
-              _hover={{
-                bgColor: "teal.700",
-              }}
-            >
-              Read More
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+              <CardFooter>
+                <Button
+                  variant="solid"
+                  bgColor={"teal"}
+                  width={"100%"}
+                  colorScheme="none"
+                  color={"white"}
+                  bottom={"0"}
+                  _hover={{
+                    bgColor: "teal.700",
+                  }}
+                >
+                  Read More
+                </Button>
+              </CardFooter>
+            </Card>
+          </Box>
+        ))}
+      </Slider>
     </>
   );
 }
--- FE/src/pages/home.tsx
+++ FE/src/pages/home.tsx
@@ -1,29 +1,33 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+  Text,
+} from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
 import FooterTemplate from "../components/Footer";
 // import { useState } from "react";
 import CardHome from "../components/Card";
 import About from "../components/About";
-import AddCardPopup from "../components/addCardPopup";
+// import AddCardPopup from "../components/addCardPopup";
+import React from "react";
+import { BiSolidImageAdd } from "react-icons/bi";
 export default function Home() {
-  // const initialCards = [
-  //   { title: 'Card 1' },
-  //   { title: 'Card 2' },
-  //   { title: 'Card 3' },
-  //   // Add more cards here
-  // ];
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // const [cards, setCards] = useState(initialCards);
-  // const [filter, setFilter] = useState<string>('');
-
-  // const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setFilter(event.target.value);
-  // };
-
-  // const filteredCards = cards.filter((card) =>
-  //   card.title.toLowerCase().includes(filter.toLowerCase())
-  // );
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
   return (
     <>
       <Box>
@@ -34,14 +38,127 @@ export default function Home() {
 
         <About />
 
+        {/* popup modal */}
         <Box display={"flex"} justifyContent={"center"}>
-          <AddCardPopup />
+          <Box width={"200px"}>
+            <Button
+              onClick={onOpen}
+              variant="solid"
+              bgColor={"teal"}
+              width={"100%"}
+              colorScheme="none"
+              color={"white"}
+              _hover={{
+                bgColor: "teal.700",
+              }}
+            >
+              Add
+            </Button>
+          </Box>
+
+          <Box>
+            <Modal
+              initialFocusRef={initialRef}
+              finalFocusRef={finalRef}
+              isOpen={isOpen}
+              onClose={onClose}
+            >
+              <ModalOverlay />
+              <ModalContent
+                bgImage={
+                  "https://ik.imagekit.io/lcfefbv0i/bg.svg?updatedAt=1696669804112"
+                }
+              >
+                <ModalHeader>
+                  <Text color={"white"}>
+                    Add New Issue in Gather
+                    <Text as="span" color={"#83b735"}>
+                      Li
+                    </Text>
+                    .
+                  </Text>
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <FormControl>
+                    <FormLabel>Title</FormLabel>
+                    <Input
+                      ref={initialRef}
+                      placeholder="Input an issue title"
+                      type="text"
+                      bg={"white"}
+                    />
+                  </FormControl>
+
+                  <FormControl mt={4}>
+                    <FormLabel>Description</FormLabel>
+                    <Input placeholder="Description" type="text" bg={"white"} />
+                  </FormControl>
+
+                  <FormControl mt={4}>
+                    <FormLabel padding={"5px "} mt={"15px"}>
+                      <Text
+                        display={"flex"}
+                        color={"white"}
+                        alignItems={"center"}
+                        textAlign={"center"}
+                        fontSize={"40px"}
+                        cursor={"pointer"}
+                      >
+                        <BiSolidImageAdd />
+                      </Text>
+                    </FormLabel>
+                    <Input
+                      placeholder="Upload Image"
+                      type="file"
+                      bg={"white"}
+                      hidden
+                    />
+                  </FormControl>
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button
+                    width={"100%"}
+                    padding={"5px 15px"}
+                    border={"1px solid teal "}
+                    borderRadius={"30px"}
+                    bg={"white"}
+                    color={"teal"}
+                    _hover={{
+                      bg: "teal",
+                      color: "white",
+                      border: "1px solid white",
+                    }}
+                    mr={3}
+                  >
+                    Post
+                  </Button>
+                  <Button
+                    onClick={onClose}
+                    width={"100%"}
+                    padding={"5px 15px"}
+                    border={"1px solid teal "}
+                    borderRadius={"30px"}
+                    bg={"white"}
+                    color={"teal"}
+                    _hover={{
+                      bg: "teal",
+                      color: "white",
+                      border: "1px solid white",
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Box>
         </Box>
 
         <Box
-          display={"grid"}
-          gridTemplateColumns={"repeat(auto-fit,minmax(270px,auto))"}
-          gap={"15px"}
+          // display={"grid"}
+          // gridTemplateColumns={"repeat(auto-fit,minmax(270px,1fr))"}
           margin={"20px 5%"}
         >
           <CardHome />
