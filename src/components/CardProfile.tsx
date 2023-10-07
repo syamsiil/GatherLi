1075a4b0b3d4e9ad5fc623e4647050e2cd0e07c7
Ahmad Syahran Zidane <zidanenoor17@gmail.com>
2023-10-07 15:33:49 +0700

    chore: refactor code

--- src/components/CardProfile.tsx
+++ src/components/CardProfile.tsx
@@ -20,14 +20,15 @@ export default function CardProfile() {
   const [selectedImage, setSelectedImage] = useState("");
 
   const handleImageUpload = (event: any) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+    const file = event.target.files[0]; //mengambil file pertama dalam array (dalam hal ini, gambar yang dipilih oleh pengguna)
+    const reader = new FileReader(); // Objek ini digunakan untuk membaca konten file yang dipilih oleh pengguna.
 
     reader.onload = (e: any) => {
+      //callback yang akan dipanggil ketika pembaca telah selesai membaca file gambar yang dipilih oleh pengguna
       setSelectedImage(e.target.result);
     };
 
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(file); // pembacaan file yang dipilih oleh pengguna sebagai data URL
   };
 
   return (
@@ -47,40 +48,61 @@ export default function CardProfile() {
                   </Heading>
                   <Flex alignItems="center">
                     <Avatar src={selectedImage} me="0.5em" />
-                    <Button as="label" htmlFor="imageUpload" cursor={"pointer"} backgroundColor={"#474fa0"} _hover={{backgroundColor: "#1c2844", textColor: "#fff"}} colorScheme="none">
+                    <Button
+                      as="label"
+                      htmlFor="imageUpload"
+                      cursor={"pointer"}
+                      backgroundColor={"#474fa0"}
+                      _hover={{ backgroundColor: "#1c2844", textColor: "#fff" }}
+                      colorScheme="none"
+                    >
                       Unggah Foto
                     </Button>
                     <input
                       id="imageUpload"
                       type="file"
-                      accept="image/*"
                       onChange={handleImageUpload}
                       style={{ display: "none" }}
                     />
                   </Flex>
-                  <FormControl isRequired>
-                    <Flex mt={"1.5em"}>
-                      <Box me={"1em"}>
-                        <FormLabel>First name</FormLabel>
-                        <Input placeholder="First name" />
+                  <form action="">
+                    <FormControl isRequired>
+                      <Flex mt={"1.5em"}>
+                        <Box me={"1em"}>
+                          <FormLabel>First name</FormLabel>
+                          <Input placeholder="First name" />
+                        </Box>
+                        <Box>
+                          <FormLabel>Last name</FormLabel>
+                          <Input placeholder="Last name" />
+                        </Box>
+                      </Flex>
+                      <Box mt={"1.5em"}>
+                        <FormLabel>Alamat Email</FormLabel>
+                        <Input type="email" placeholder="Email" />
                       </Box>
-                      <Box>
-                        <FormLabel>Last name</FormLabel>
-                        <Input placeholder="Last name" />
+                      <Box mt={"1.5em"}>
+                        <FormLabel>Biografi</FormLabel>
+                        <Textarea resize={"none"} height={"200px"} />
                       </Box>
-                    </Flex>
-                    <Box mt={"1.5em"}>
-                      <FormLabel>Alamat Email</FormLabel>
-                      <Input type="email" placeholder="Email" />
-                    </Box>
-                    <Box mt={"1.5em"}>
-                      <FormLabel>Biografi</FormLabel>
-                      <Textarea resize={"none"} height={"200px"} />
-                    </Box>
-                    <Flex justifyContent={"end"} mt={"1.5em"} cursor={"pointer"}>
-                      <Button backgroundColor={"#474fa0"} _hover={{backgroundColor: "#1c2844", textColor: "#fff"}} colorScheme="none">Simpan</Button>
-                    </Flex>
-                  </FormControl>
+                      <Flex
+                        justifyContent={"end"}
+                        mt={"1.5em"}
+                        cursor={"pointer"}
+                      >
+                        <Button
+                          backgroundColor={"#474fa0"}
+                          _hover={{
+                            backgroundColor: "#1c2844",
+                            textColor: "#fff",
+                          }}
+                          colorScheme="none"
+                        >
+                          Simpan
+                        </Button>
+                      </Flex>
+                    </FormControl>
+                  </form>
                 </Box>
               </CardBody>
             </Card>
---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,8 @@
+import { FormEvent } from "react";
+
+async function handlePost(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    
+}
